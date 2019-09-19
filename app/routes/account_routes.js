35303caaa1cb3bbd196f61652ba1df85b95fb9fb
IDemetrus <workhome.pro@gmail.com>
2019-09-19 20:35:00 +0300

    Set CRUD requests

--- app/routes/account_routes.js
+++ app/routes/account_routes.js
@@ -1,17 +1,32 @@
 module.exports = function(app, database){
+    //Define our database
+    const ourDB = database.db('db')
+    
     app.post('/v1/account/register', (request, respond) => {
+        const randNumber = Math.random().toString()
+        const token = randNumber.slice(2)
         //Here create account
         const account = { 
             email: request.body.email, 
-            password: request.body.password 
+            password: request.body.password,
+            token: token 
         }
-        const ourDB = database.db('db')
         ourDB.collection('accounts').insertOne(account, (err, result)=>{
             if(err){
                 respond.send({'error':'An error has occurred'});
             }else{
                 respond.send(result.ops[0])
             }
+        })
+    })
+    app.post('/v1/account/login', (request, respond) => {
+        const account = {
+            email: request.body.email,
+            password: request.body.password
+        }
+        ourDB.collection('accounts').findOne(account, (err, result) => {
+            if(err) throw err
+            respond.send(result)
         })
     })
 }
