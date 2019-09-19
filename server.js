468f3520642e1a1699bc0e46e707150719650e9e
IDemetrus <workhome.pro@gmail.com>
2019-09-19 12:34:00 +0300

    install express, body-parser, nodemon

---
+++ server.js
@@ -0,0 +1,11 @@
+// Server
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const app = express();
+
+//Turn on server
+const port = 8000;
+app.listen(port, () => {
+    console.log("We are live on "+ port);
+})
