d7f9b4e87d994054c4b2d41c46dac07e68789fb6
IDemetrus <workhome.pro@gmail.com>
2019-09-20 20:57:22 +0300

    add auth & changed tree

--- server.js
+++ server.js
@@ -1,39 +1,28 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
-// Create express app
-const app = express()
+const userRouter = require('./app/routers/user.router')
 // Use PORT
-const port = process.env.PORT || 3005
-
-// Parse content of "applicaton/json"
-app.use(bodyParser.json())
+const port = process.env.PORT
 
 // ---------------- Database ---------------- \\
 // Init database
-const dbConfig = require('./config/database.config')
-const mongoose = require('mongoose')
-mongoose.Promise = global.Promise
+require('./app/config/db')
 
-// Connect to database
-mongoose.connect(dbConfig.url, { 
-    useNewUrlParser : true, 
-    useUnifiedTopology : true 
-}).then(() => { 
-    'Successfully connected to the database' 
-}).catch(err => { 
-    console.log('Could not connect to the db', err)
-    process.exit() 
-})
 // ---------------- Database ---------------- \\
+
+// Create express app
+const app = express()
+
+// Parse content of "applicaton/json"
+app.use(bodyParser.json())
 
 // Define a starting route
 app.get('/', (req, res) => {
     res.status(201).json({'message':'Server is online.'})
 })
 
-// Add User routes
-require('./app/routes/user.routes')(app)
+// Init User routes
+app.use(userRouter)
 
 // Run the server to listen for requests
 app.listen(port, () => { 
